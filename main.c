e29a5be8cbee63086e3f35390b73a10725aaaa1b
subedinab <79269587+subedinab@users.noreply.github.com>
2021-12-17 19:06:09 +0545

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//the changes have been made here just for checking purpose.
+//the changes have been made here just for checking purpose plus this is done from github.
  static int fun(int a,int b){
 	int c;
 	c=a+b;
